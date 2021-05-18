c39af201d74114ddd2d0e6b39bcf828ffe243d46
Th3nn3ss <chuksmcdennis@yahoo.com>
2021-05-18 14:00:53 +0100

    Improved Tests
    Added more test cases

--- Dennis_hashtable/hashtable.py
+++ Dennis_hashtable/hashtable.py
@@ -15,17 +15,25 @@ Item = namedtuple('Item', ['key', 'value'])
 class HashTable:
     ''' A HashTable implementation the handles collision and dynamic resizing
     '''
+    # An init function with an initial size of 8
+    # With a load factor of 0.8, which is 80% of the current number of slots
 
     def __init__(self, size=8, load_factor=0.8):
         self._slots = [[] for i in range(size)]
-        self._count = 0
+        self._count = 0  # To keep count of the number of Table items
         self._load_factor = load_factor
 
+    # A method that returns the number of items in the table
     def __len__(self):
         return self._count
 
+    # A method to handle indexing a key using unique hashing
+
     def _index(self, key):
         return hash(key) % len(self._slots)
+
+    # A method to handle adding new items to the Table
+    # It also takes care of handling collisions when two key's are of the same hashed index
 
     def add_item(self, key, value):
         index = self._index(key)
@@ -34,17 +42,30 @@ class HashTable:
             if item.key == key:
                 this_slot[item_index] = item._replace(value=value)
                 break
+        # when the for loop is not executed it means this slot is currently empty
+        # the else clause appends the (key, value) item to the slot.
         else:
             item = Item(key, value)
             self._slots[index].append(item)
-            self._count = self._count + 1
+            self._count = self._count + 1  # Count is increased by 1
+
+        # The _resize method is triggered when ever the load factor is reached or passed.
+        # This is to create more available slots when the capacity is at 80%
         if len(self)/len(self._slots) >= self._load_factor:
             self._resize()
+
+    # A class method for implementing an insert into the newly resized larger slots.
 
     @classmethod
     def _insert(cls, slots, key, value):
         index = cls._index(key, slots)
         slots[index] = value
+
+    # A Generator method that generates every item in the Table
+    def items(self):
+        yield from itertools.chain.from_iterable(self._slots)
+
+    # A method for resizing the Table to 2 times it's original avaliable slots.
 
     def _resize(self):
         larger_slots = [[] for i in range(len(self._slots)*2)]
@@ -52,12 +73,18 @@ class HashTable:
             HashTable._insert(larger_slots, key, value)
         self._slots = larger_slots
 
+    # A method for getting an item from the table using it's key
     def get_item(self, key):
         index = self._index(key)
         for element in self._slots[index]:
             if element.key == key:
                 return element.value
+
+        # If the key is not an item in the Table.
+        # A personallized error message is returned.
         return "'{}' is not a key in this HashTable".format(key)
+
+    # A method for handling deleting an Item from the table using it's key
 
     def del_item(self, key):
         index = self._index(key)
@@ -66,6 +93,8 @@ class HashTable:
             if item.key == key:
                 del this_slot[item_index]
                 break
+        # If the key is not an item in the Table.
+        # A personallized error message is returned.
         else:
             return "'{}' is not a key in this HashTable".format(key)
-        self._count = self._count - 1
+        self._count = self._count - 1  # Count is decreased by 1.
--- Dennis_hashtable/test_hashtable.py
+++ Dennis_hashtable/test_hashtable.py
@@ -9,21 +9,26 @@ from hashtable import HashTable
 
 class TestHashTable(unittest.TestCase):
 
+    # Initialize an object name hasher
     def setUp(self):
         self.hasher = HashTable()
 
+    # Test for Add Item method and Delete method.
     def test_add_item(self):
         self.hasher.add_item("one", 1)
         self.assertEqual(self.hasher.get_item("one"), 1)
         self.hasher.add_item("one", "unos")
         self.assertEqual(self.hasher.get_item("one"), "unos")
 
+    # Test for Add Item method and Get Item method
     def test_del(self):
         self.hasher.add_item("two", 2)
         self.hasher.add_item("three", 3)
         self.hasher.del_item("two")
         self.assertEqual(self.hasher.get_item("two"),
                          "'two' is not a key in this HashTable")
+
+    # Test for length, Add Item and Delete Item methods
 
     def test_len(self):
         self.hasher.add_item("ten", 10)
@@ -32,6 +37,13 @@ class TestHashTable(unittest.TestCase):
         self.assertEqual(len(self.hasher), 3)
         self.hasher.del_item(10)
         self.assertEqual(len(self.hasher), 2)
+
+    # Test the yeild generator as used in the Items method
+    def test_items(self):
+        self.hasher.add_item("This ", "is ")
+        for key, value in self.hasher.items():
+            self.assertEqual(key, "This ")
+            self.assertEqual(value, "is ")
 
 
 if __name__ == "__main__":
