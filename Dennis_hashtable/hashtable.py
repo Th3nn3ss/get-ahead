fa301f1ebb98ae6dcc0a1b80901798a24ea5d707
Th3nn3ss <chuksmcdennis@yahoo.com>
2021-06-09 20:09:36 +0100

    Fixed typos Conducted more tests Followed some recommendation from Google's Python Style Guide.

--- Dennis_hashtable/hashtable.py
+++ Dennis_hashtable/hashtable.py
@@ -4,99 +4,111 @@
 # Email: chuksmcdennis@yahoo.com
 # Gmail: chuksmcdennis11@gmail.com
 
-''' An attempt at a simple Hash Table using only Arrays(lists) '''
+''' Hash Table using only Python lists '''
 
 from collections import namedtuple
+import itertools
 
-Item = namedtuple('Item', ['key', 'value'])
+Entry = namedtuple('Entry', ['key', 'value'])
 
 
-class HashTable:
-    ''' A HashTable implementation the handles collision and dynamic resizing
+class Table:
+    ''' A Python Hash Table 
+    To use
+    >>> ht = Table()
+    >>> ht["one"] =  1
+    >>> ht["one"]
+    1
+    >>> len(ht)
+    1
+    >>> del ht["one"]
+    >>> len(ht)
+    0
+
     '''
-    # An init function with an initial size of 8
-    # With a load factor of 0.8, which is 80% of the current number of slots
 
     def __init__(self, size=8, load_factor=0.8):
-        self._slots = [[] for i in range(size)]
-        self._count = 0  # To keep count of the number of Table items
+        ''' Initialize a Hash Table with an initial size of 8, a load factor of 0.8 '''
+        self._slots = [[] for _ in range(size)]
+        self._size = 0
         self._load_factor = load_factor
 
-    # A method that returns the number of items in the table
     def __len__(self):
-        return self._count
+        ''' A method that returns the number of items in the table '''
+        return self._size
 
-    # A method to handle indexing a key using unique hashing
     @staticmethod
     def _index(key, slots):
+        ''' A method to handle indexing a key using unique hashing 
+        Args:
+            key
+            slots
+        Return:
+           Unique hash for key 
+        '''
         return hash(key) % len(slots)
 
-    # A method to handle adding new items to the Table
-    # It also takes care of handling collisions when two key's are of the same hashed index
-
-    def add_item(self, key, value):
+    def __setitem__(self, key, value):
+        ''' A method to handle adding new items to the Table '''
         index = self._index(key, self._slots)
-        this_slot = self._slots[index]
-        for item_index, item in enumerate(this_slot):
-            if item.key == key:
-                this_slot[item_index] = item._replace(value=value)
-                break
-        # when the for loop is not executed it means this slot is currently empty
-        # the else clause appends the (key, value) item to the slot.
-        else:
-            item = Item(key, value)
-            self._slots[index].append(item)
-            self._count = self._count + 1  # Count is increased by 1
+        slot = self._slots[index]
 
-        # The _resize method is triggered when ever the load factor is reached or passed.
-        # This is to create more available slots when the capacity is at 80%
+        for item_index, item in enumerate(slot):
+            if item.key == key:
+                slot[item_index] = item._replace(value=value)
+                break
+
+        # When the for loop is not executed it means this slot is currently empty
+        else:
+            entry = Entry(key, value)
+            self._slots[index].append(entry)
+            self._size += 1
+
+        # The _resize method is triggered when ever the load factor is reached to create more available slots
         if len(self)/len(self._slots) >= self._load_factor:
             self._resize()
 
-    # A class method for implementing an insert into the newly resized larger slots.
-
     @classmethod
     def _insert(cls, slots, key, value):
+        ''' A class method for implementing an insert into the newly resized larger slots. '''
         index = cls._index(key, slots)
-        item = Item(key, value)
-        slots[index].append(item)
+        entry = Entry(key, value)
+        slots[index].append(entry)
 
-    # A Generator method that generates every item in the Table
     def items(self):
-        for slot in self._slots:
-            for item in slot:
-                yield item
-
-    # A method for resizing the Table to 2 times it's original avaliable slots.
+        ''' A method that generates every item in the Table '''
+        yield from itertools.chain.from_iterable(self._slots)
 
     def _resize(self):
+        ''' A method for resizing the Table to 2 times it's original avaliable slots. '''
         larger_slots = [[] for i in range(len(self._slots)*2)]
+
         for key, value in self.items():
-            HashTable._insert(larger_slots, key, value)
+            Table._insert(larger_slots, key, value)
         self._slots = larger_slots
 
-    # A method for getting an item from the table using it's key
-    def get_item(self, key):
+    def __getitem__(self, key):
+        ''' A method for getting an item from the table using it's key '''
         index = self._index(key, self._slots)
+
         for element in self._slots[index]:
             if element.key == key:
                 return element.value
 
         # If the key is not an item in the Table.
-        # A personallized error message is returned.
-        return "'{}' is not a key in this HashTable".format(key)
+        raise KeyError(key)
 
-    # A method for handling deleting an Item from the table using it's key
-
-    def del_item(self, key):
+    def __delitem__(self, key):
+        ''' A method for handling deleting an Item from the table using it's key '''
         index = self._index(key, self._slots)
-        this_slot = self._slots[index]
-        for item_index, item in enumerate(this_slot):
+        slot = self._slots[index]
+
+        for item_index, item in enumerate(slot):
             if item.key == key:
-                del this_slot[item_index]
+                del slot[item_index]
                 break
-        # If the key is not an item in the Table.
-        # A personallized error message is returned.
+
+        # If the key is not present
         else:
-            return "'{}' is not a key in this HashTable".format(key)
-        self._count = self._count - 1  # Count is decreased by 1.
+            raise KeyError(key)
+        self._size -= 1
--- Dennis_hashtable/test_hashtable.py
+++ Dennis_hashtable/test_hashtable.py
@@ -1,59 +1,59 @@
 #! /usr/bin/env python3
 
-''' Test for the HashTable class using unittest '''
+''' Test for the Table class using unittest '''
 
 
 import unittest
-from hashtable import HashTable
+from hashtable import Table
 
 
-class TestHashTable(unittest.TestCase):
+class TestTable(unittest.TestCase):
 
     # Initialize an object name hasher
     def setUp(self):
-        self.hasher = HashTable()
+        self.hasher = Table()
 
     # Test for Add Item method and Delete method.
     def test_add_item(self):
-        self.hasher.add_item("one", 1)
-        self.assertEqual(self.hasher.get_item("one"), 1)
-        self.hasher.add_item("one", "unos")
-        self.assertEqual(self.hasher.get_item("one"), "unos")
+        self.hasher["one"] = 1
+        self.assertEqual(self.hasher["one"], 1)
+        self.hasher["one"] = "unos"
+        self.assertEqual(self.hasher["one"], "unos")
 
     # Test for Add Item method and Get Item method
     def test_del(self):
-        self.hasher.add_item("two", 2)
-        self.hasher.add_item("three", 3)
-        self.hasher.del_item("two")
-        self.assertEqual(self.hasher.get_item("two"),
-                         "'two' is not a key in this HashTable")
+        self.hasher["two"] = 2
+        self.hasher["three"] = 3
+        del self.hasher["two"]
+        with self.assertRaises(KeyError):
+            self.hasher["two"]
 
     # Test for length, Add Item and Delete Item methods
 
     def test_len(self):
-        self.hasher.add_item("ten", 10)
-        self.hasher.add_item(10, "ten")
-        self.hasher.add_item("eleven", 11)
+        self.hasher["ten"] = 10
+        self.hasher[10] = "ten"
+        self.hasher["eleven"] = 11
         self.assertEqual(len(self.hasher), 3)
-        self.hasher.del_item(10)
+        del self.hasher[10]
         self.assertEqual(len(self.hasher), 2)
 
-    # Test the yeild generator as used in the Items method
-    def test_items(self):
-        self.hasher.add_item("This ", "is ")
-        for key, value in self.hasher.items():
-            self.assertEqual(key, "This ")
-            self.assertEqual(value, "is ")
-
     # Forther tests to check if the resizing is efficient
+
     def test_resizing_(self):
         array = [1, 2, 3, 4, 5, 5, 2, 3, 2, 3, 2, 3, 2, 3,
                  2, 3, 4, 2, 3, 4, 3, 2, 4, 3, 2, 4, 2, 3, 4, 3]
+
         for index, value in enumerate(array):
-            self.hasher.add_item(index, value)
-        # Since the initial slots of the slot is 8
-        # Then is test should test if the slots was resized twice
+            self.hasher[index] = value
+
+        # Since the initial available slots of the Table is 8
+        # Then this test should test if the Table was resized twice
         self.assertEqual(len(self.hasher), 30)
+
+        # To test for every object inserted into the Table
+        for index, value in enumerate(array):
+            self.assertEqual(self.hasher[index], value)
 
 
 if __name__ == "__main__":
